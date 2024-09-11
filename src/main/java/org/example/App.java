24594dcfc9b136a250d57679f3588e7c29d02ab8
AbdifatahAbdulkadir <abdifatah.abdulakadir@iths.se>
2024-09-11 11:52:05 +0200

    Print menu

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,6 +3,22 @@ package org.example;
 public class App {
 
     public static void main(String[] args) {
+        String t = """
+                Elpriser
+                ========
+                1. Inmatning
+                2. Min, Max och Medel
+                3. Sortera
+                4. Bästa Laddningstid (4h)
+                e. Avsluta
+                
+                
+                """;
+        System.out.print(t);
+
+
+
+
         System.out.println("Hello There!");
     }
 }
